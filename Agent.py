cc1c1b69abcffb0e379eb6ce18fab34bbf796aef
Markus Andresen <markus.andresen94@gmail.com>
2017-11-21 16:28:31 +0100

    tried som shit

--- Agent.py
+++ Agent.py
@@ -28,10 +28,13 @@ class Agent:
     def __init__(self, env, n_observations: int, n_actions: int):
         self.environment = env
         self.gamma = 0.95
+        self.epsilon = 1.0
+        self.epsilon_min = 0.01
+        self.epsilon_decay = 0.995
         self.batch_size = 32
         self.n_observations = n_observations
         self.last_state = self.environment.reset().reshape(1, n_observations)
-        self.memory = deque(maxlen=1000)
+        self.memory = deque(maxlen=2000)
         self.Q_network = build_model(n_inputs=n_observations, n_outputs=n_actions)
 
     # Replaced by random sample ?
@@ -43,10 +46,15 @@ class Agent:
         return results
 
     def get_next_action(self):
-        output = self.Q_network.predict(self.last_state)
-        return np.random.choice(range(0, self.environment.action_space.n), p=output[0])
+        self.epsilon *= self.epsilon_decay
+        self.epsilon = max(self.epsilon_min, self.epsilon)
+        if np.random.random() < self.epsilon:
+            return self.environment.action_space.sample()
+        return np.argmax(self.Q_network.predict(self.last_state)[0])
+        # return np.random.choice(range(0, self.environment.action_space.n), p=output[0])
 
     def train(self):
+        # TODO: FIND BUG AND REMOVE IT
         if len(self.memory) >= self.batch_size:
             samples = random.sample(self.memory, self.batch_size)
             inputs = []
@@ -62,17 +70,19 @@ class Agent:
                     output[0][action] = q_current
                 inputs.append(state[0])
                 targets.append(output[0])
+            print(targets)
             self.Q_network.fit(np.array(inputs), np.array(targets), batch_size=1, epochs=1, verbose=0)
 
     def run(self):
         self.last_state = self.environment.reset().reshape(1, self.n_observations)
         done = False
         total_reward = 0
+        print("Epsilon", self.epsilon)
         while not done:
             action = self.get_next_action()
             state, reward, done, _ = self.environment.step(action)
             total_reward += reward
-            self.memory.append((self.last_state, action, total_reward, state, done))
+            self.memory.append((self.last_state, action, reward, state, done))
             self.train()
             self.last_state = state.reshape(1, self.n_observations)
         return total_reward
